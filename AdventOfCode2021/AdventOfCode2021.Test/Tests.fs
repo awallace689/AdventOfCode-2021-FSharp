74147b54e91bf80ad770327e7ee0983a5d776a40
awallace689 <awallace689@gmail.com>
2021-12-02 23:37:47 -0600

    fix tests

--- AdventOfCode2021/AdventOfCode2021.Test/Tests.fs
+++ AdventOfCode2021/AdventOfCode2021.Test/Tests.fs
@@ -4,9 +4,16 @@ open NUnit.Framework
 
 [<Test>]
 let Day1P1_Answer_Correct () =
-    Assert.That(Day1.P1.run(), Is.EqualTo(1451), "Day1P1 - answer matches site answer")
-    
+    Assert.That(
+        Day1.P1.run (),
+        Is.EqualTo(1451),
+        "Day1P1 - correct number of adjacent numbers found in list where fst < snd"
+    )
+
 [<Test>]
 let Day1P2_Answer_Correct () =
-    Assert.That(Day1.P2.run(), Is.EqualTo(0))
-    
+    Assert.That(
+        Day1.P2.run (),
+        Is.EqualTo(1395),
+        "Day1P2 - correct number of increasing, adjacent sums of 3-number slices in list found"
+    )
