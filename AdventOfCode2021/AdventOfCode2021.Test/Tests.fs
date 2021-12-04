39f3034319678b359f22d316c984865db4b0d486
Adam Wallace <awallace689@gmail.com>
2021-12-04 17:07:12 -0600

    day2 tests

--- AdventOfCode2021/AdventOfCode2021.Test/Tests.fs
+++ AdventOfCode2021/AdventOfCode2021.Test/Tests.fs
@@ -17,3 +17,19 @@ let Day1P2_Answer_Correct () =
         Is.EqualTo(1395),
         "Day1P2 - correct number of increasing, adjacent sums of 3-number slices in list found"
     )
+    
+[<Test>]
+let Day2P1_Answer_Correct () =
+    Assert.That(
+        Day2.P1.run (),
+        Is.EqualTo(1561344),
+        "Day2P1 - correct final position of submarine, calculated w/ x * y"
+    )
+    
+[<Test>]
+let Day2P2_Answer_Correct () =
+    Assert.That(
+        Day2.P1.run (),
+        Is.EqualTo(1848454425),
+        "Day2P2 - correct final position of submarine, calculated w/ aim"
+    )
